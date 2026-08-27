f269654c9131cecfaf713728c576410118a73c69
venkatkushal18 <venkatkushal18@gmail.com>
2026-08-27 19:25:00 +0530

    Add files via upload

---
+++ day 18 q2.c
@@ -0,0 +1,33 @@
+/*Q36: Write a program to find the HCF (GCD) of two numbers.
+
+
+Sample Test Cases:
+Input 1:
+12 18
+Output 1:
+6
+
+Input 2:
+7 9
+Output 2:
+1
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("%d", a);
+
+    return 0;
+}
