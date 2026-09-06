4a086c81f0d12de53401a0b4e65d65f5de4f3ff1
venkatkushal18 <venkatkushal18@gmail.com>
2026-09-07 00:00:14 +0530

    Add files via upload

---
+++ day 28 q2.c
@@ -0,0 +1,41 @@
+/*Q56: Read and print elements of a one-dimensional array.
+
+
+Sample Test Cases:
+Input 1:
+3
+10 20 30
+Output 1:
+10 20 30
+
+Input 2:
+5
+1 2 3 4 5
+Output 2:
+1 2 3 4 5
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int a[100];
+
+    scanf("%d", &n);
+
+    // Read array elements
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    // Print array elements
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
